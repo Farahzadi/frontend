0e688953716ee1b0f63723809872e91824ccb24e
Nathan Franken5tein <franken5tein0@proton.me>
2022-10-23 17:35:27 +0330

    Feature(app): Add api to the lib

---
+++ src/app/src/lib/api/providers/APIProvider.js
@@ -0,0 +1,25 @@
+const notImplemented = function (method) {
+    const x = () => { throw new Error(`APIProvider method not implemented: ${method}`) }
+    x.notImplemented = true
+    return x
+}
+
+export default class APIProvider {
+    // Methods required to be implemented
+    signIn          = notImplemented('signIn')
+    signOut         = notImplemented('signOut')
+    getAccountState = notImplemented('getAccountState')
+    submitOrder     = notImplemented('submitOrder')
+    submitSwap      = notImplemented('submitSwap')
+    depositL2       = notImplemented('depositL2')
+    withdrawL2      = notImplemented('withdrawL2')
+    depositL2Fee    = notImplemented('depositL2Fee')
+    withdrawL2Fee   = notImplemented('withdrawL2Fee')
+    getBalances     = notImplemented('getBalances')
+    getProfile      = notImplemented('getProfile')
+
+    constructor(api, network) {
+        this.api = api
+        this.network = network
+    }
+}
