833b1d90634a8b2d4800662ee547e68bc1004379
Stephen Hawking <WilliamHawkingStephen@proton.me>
2022-11-19 18:06:10 +0330

    Feature(app): add new format of app.js with cache hanlding

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,20 +1,25 @@
 import AppRoutes from "AppRoutes";
 import React from "react";
+
 import Provider from "lib/Provider";
 import api from "lib/api";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "App.css";
+import clearCache from "./cacheHandler";
 
 api.start();
 
-class App extends React.Component {
-  render() {
-    return (
-      <Provider>
-        <AppRoutes />
-      </Provider>
-    );
-  }
-}
+const main = () => {
+  return (
+    <Provider>
+      <AppRoutes />
+    </Provider>
+  );
+};
+const ClearCacher = clearCache(main);
+
+const App = () => {
+  return <ClearCacher/>;
+};
 
 export default App;
