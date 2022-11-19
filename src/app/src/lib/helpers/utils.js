08b7bcbd40304bfc64cf8368a0b2b7b8be5659ce
Stephen Hawking <WilliamHawkingStephen@proton.me>
2022-11-19 17:14:57 +0330

    Feature(app): add time converter (time -> epoch format)

--- src/app/src/lib/helpers/utils.js
+++ src/app/src/lib/helpers/utils.js
@@ -1,5 +1,14 @@
+import moment from "moment";
 
-export function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+export const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
+/**
+ * Function returning the build date(as per provided epoch)
+ * @param date Time in milliseconds
+ */
+export const getBuildDate = (date) => {
+  const buildDate = moment(date).format("DD-MM-YYY HH:MM");
+  return buildDate;
+};
