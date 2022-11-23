a60b15d719aa699dd74110c67be151ff1cb66958
0xfadecafe <0xfadecafe@proton.me>
2022-11-23 18:30:13 +0330

    Refactor(app): added State sub-class to APIProvider interface (to store APIProvider state) added start and stop methods to APIProvider interface (for API.js to start and stop a specific api-provider) added signMessage, verifyMessage and switchNetwork methods to APIProvider interface

--- src/app/src/lib/api/providers/APIProvider.js
+++ src/app/src/lib/api/providers/APIProvider.js
@@ -1,25 +1,70 @@
 const notImplemented = function (method) {
-    const x = () => { throw new Error(`APIProvider method not implemented: ${method}`) }
-    x.notImplemented = true
-    return x
-}
+  const x = () => {
+    throw new Error(`APIProvider method not implemented: ${method}`);
+  };
+  x.notImplemented = true;
+  return x;
+};
 
 export default class APIProvider {
-    // Methods required to be implemented
-    signIn          = notImplemented('signIn')
-    signOut         = notImplemented('signOut')
-    getAccountState = notImplemented('getAccountState')
-    submitOrder     = notImplemented('submitOrder')
-    submitSwap      = notImplemented('submitSwap')
-    depositL2       = notImplemented('depositL2')
-    withdrawL2      = notImplemented('withdrawL2')
-    depositL2Fee    = notImplemented('depositL2Fee')
-    withdrawL2Fee   = notImplemented('withdrawL2Fee')
-    getBalances     = notImplemented('getBalances')
-    getProfile      = notImplemented('getProfile')
+  static State = class {
+    static DISCONNECTED = "DISCONNECTED";
+    static CONNECTING = "CONNECTING";
+    static CONNECTED = "CONNECTED";
+    static SWITCHING = "SWITCHING";
+    static DISCONNECTING = "DISCONNECTING";
 
-    constructor(api, network) {
-        this.api = api
-        this.network = network
-    }
+    _state = "DISCONNECTED";
+
+    onChange;
+
+    set = (state) => {
+      if (!Object.keys(APIProvider.State).includes(state)) return;
+      const prev = this._state;
+      this._state = state;
+      if (prev !== state) this.onChange?.(state);
+    };
+
+    get = () => {
+      return this._state;
+    };
+  };
+
+  /// Common fields
+
+  // network name used in backend api calls
+  NETWORK = "ethereum";
+  // network name used in web3modal
+  NETWORK_NAME = "mainnet";
+  // address of smart contract used for bridge functionality
+  BRIDGE_CONTRACT = "0x0000000000000000000000000000000000000000";
+
+  state = new APIProvider.State();
+
+  /// Methods required to be implemented
+
+  start = notImplemented("start");
+  stop = notImplemented("stop");
+  signIn = notImplemented("signIn");
+  signOut = notImplemented("signOut");
+  getAccountState = notImplemented("getAccountState");
+  submitOrder = notImplemented("submitOrder");
+  submitSwap = notImplemented("submitSwap");
+  depositL2 = notImplemented("depositL2");
+  withdrawL2 = notImplemented("withdrawL2");
+  depositL2Fee = notImplemented("depositL2Fee");
+  withdrawL2Fee = notImplemented("withdrawL2Fee");
+  getBalances = notImplemented("getBalances");
+  getProfile = notImplemented("getProfile");
+
+  signMessage = notImplemented("signMessage");
+  verifyMessage = notImplemented("verifyMessage");
+  switchNetwork = notImplemented("switchNetwork");
+
+  onAccountChange = notImplemented("onAccountChange");
+
+  constructor(api, onStateChange) {
+    this.api = api;
+    this.state.onChange = onStateChange;
+  }
 }
