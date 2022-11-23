532c6205c3547e79a157f5dfe81d6c4fae02e9b7
0xfadecafe <0xfadecafe@proton.me>
2022-11-23 18:45:25 +0330

    Refactor(app): added setting the default network functionality on empty rehydration (empty local storage for persist)

--- src/app/src/lib/store/features/auth/authSaga.js
+++ src/app/src/lib/store/features/auth/authSaga.js
@@ -1,28 +1,33 @@
-import { REHYDRATE } from 'redux-persist'
-import { takeEvery, select, apply, all } from 'redux-saga/effects'
-import api from 'lib/api'
+import { REHYDRATE } from "redux-persist";
+import { takeEvery, select, apply, all } from "redux-saga/effects";
+import api from "lib/api";
 
-function *handleHydration({ payload }) {
+const DEFAULT_NETWORK = process.env.REACT_APP_DEFAULT_NETWORK;
+
+function* handleHydration({ payload, key }) {
+  if (key === "api") {
     if (payload && payload.network) {
-        const user = yield select(state => state.auth && state.auth.user)
-        api.setAPIProvider(payload.network)
-        
-        if (user && user.id) {
-            try {
-                yield apply(api, api.signIn, [payload.network])
-            } catch (err) {
-                console.log('There was an error reauthenticating', err)
-            }
+      const user = yield select((state) => state.auth?.user);
+      api.setNetwork(payload.network.name);
+
+      if (user?.id) {
+        try {
+          yield apply(api, api.signIn, [payload.network.name]);
+        } catch (err) {
+          console.log("There was an error reauthenticating", err);
         }
+      }
+    } else {
+      console.log(`Switching to default network "${DEFAULT_NETWORK}"`);
+      api.setNetwork(DEFAULT_NETWORK);
     }
+  }
 }
 
-export function *authHandlerSaga() {
-    yield takeEvery(REHYDRATE, handleHydration)
+export function* authHandlerSaga() {
+  yield takeEvery(REHYDRATE, handleHydration);
 }
 
-export default function *authSaga() {
-    yield all([
-        authHandlerSaga(),
-    ])
+export default function* authSaga() {
+  yield all([authHandlerSaga()]);
 }
