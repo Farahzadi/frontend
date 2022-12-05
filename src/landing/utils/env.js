b0313739b214aba4e40c28d0f49f294e8ee28cdb
nearsilen <nearsilen@proton.me>
2022-12-05 14:19:36 +0330

    Fix(landing): add static value of trade and docs url

--- src/landing/utils/env.js
+++ src/landing/utils/env.js
@@ -1,16 +1,16 @@
 const getDocsLink = () => {
-  return process.env.NEXT_PUBLIC_DOCS_LINK || '';
+  return process.env.NEXT_PUBLIC_DOCS_LINK || 'https://docs.dexpresso.exchange';
 };
 const getAPIUrl = () => {
-  return process.env.APP_BACKEND_API || '';
+  return process.env.APP_BACKEND_API || 'https://api.dexpresso.exchange/api/v1';
 };
 
 const getBaseUrl = () => {
-  return process.env.BASE_URL || '';
+  return process.env.BASE_URL || 'https://dexpresso.exchange';
 };
 
 const getTradeLink = () => {
-  return process.env.NEXT_PUBLIC_TRADE_APP_LINK || '';
+  return process.env.NEXT_PUBLIC_TRADE_APP_LINK || 'https://trade.dexpresso.exchange';
 };
 
 export { getAPIUrl, getBaseUrl, getDocsLink, getTradeLink };
