58956386cc278a7ddad2bbe3c4dcbb8529b41dc3
nearsilen <nearsilen@proton.me>
2022-12-06 16:49:43 +0330

    Fix(landing): remove static url values

--- src/landing/utils/env.js
+++ src/landing/utils/env.js
@@ -1,16 +1,16 @@
 const getDocsLink = () => {
-  return process.env.NEXT_PUBLIC_DOCS_LINK || 'https://docs.dexpresso.exchange';
+  return process.env.NEXT_PUBLIC_DOCS_LINK || '';
 };
 const getAPIUrl = () => {
-  return process.env.APP_BACKEND_API || 'https://api.dexpresso.exchange/api/v1';
+  return process.env.APP_BACKEND_API || '';
 };
 
 const getBaseUrl = () => {
-  return process.env.BASE_URL || 'https://dexpresso.exchange';
+  return process.env.BASE_URL || '';
 };
 
 const getTradeLink = () => {
-  return process.env.NEXT_PUBLIC_TRADE_APP_LINK || 'https://trade.dexpresso.exchange';
+  return process.env.NEXT_PUBLIC_TRADE_APP_LINK || '';
 };
 
 export { getAPIUrl, getBaseUrl, getDocsLink, getTradeLink };
