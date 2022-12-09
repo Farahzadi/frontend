0b833bf26ac7e5510c80d5ee02ffa0c97ff93bc0
Stephen Hawking <WilliamHawkingStephen@proton.me>
2022-12-09 17:46:55 +0330

    Refactor(app): remove old cache handler logic

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -5,21 +5,15 @@ import Provider from "lib/Provider";
 import api from "lib/api";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "App.css";
-import clearCache from "./cacheHandler";
 
 api.start();
 
-const main = () => {
+const App = () => {
   return (
     <Provider>
       <AppRoutes />
     </Provider>
   );
-};
-const ClearCacher = clearCache(main);
-
-const App = () => {
-  return <ClearCacher/>;
 };
 
 export default App;
