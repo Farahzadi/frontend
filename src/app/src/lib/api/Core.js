8877b127b49d4a24a508ec357cffe81b3c3e1c46
0xfadecafe <0xfadecafe@proton.me>
2022-12-12 14:20:43 +0330

    Refactor(app): changed Core properties into methods added new Action list for UI to use added run method to run actions moved out unrelated methods

--- src/app/src/lib/api/Core.js
+++ src/app/src/lib/api/Core.js
@@ -1,17 +1,26 @@
 import Decimal from "decimal.js";
-import { createIcon } from "@download/blockies";
 import Emitter from "tiny-emitter";
-
-import { getENSName } from "lib/ens";
-import { formatAmount } from "lib/utils";
-import erc20ContractABI from "lib/contracts/ERC20.json";
-import { maxAllowance } from "./constants";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 const DEFAULT_NETWORK = process.env.REACT_APP_DEFAULT_NETWORK;
 
 export default class Core extends Emitter {
+  static Actions = [
+    "start",
+    "stop",
+    "setNetwork",
+    "subscribeToMarket",
+    "unsubscribeToMarket",
+    "getMarketConfig",
+    "getMarketInfo",
+    "cancelOrder",
+    "cancelAllOrders",
+    "getOrderBook",
+    "submitOrder",
+    "getNetworks",
+  ];
+
   networkClasses = {};
   network = DEFAULT_NETWORK || "zksyncv1_goerli";
   networkInterface = null;
@@ -48,7 +57,7 @@ export default class Core extends Emitter {
     this.signInMessage = signInMessage ?? "Login to Dexpresso";
   }
 
-  setNetwork = async (network) => {
+  async setNetwork(network) {
     if (this.network === network) return;
     await this.stopNetworkInterface();
     try {
@@ -62,82 +71,37 @@ export default class Core extends Emitter {
       name: network,
       ...this.networkInterface.getConfig(),
     });
-  };
+  }
 
-  connectWallet = async () => {
+  async connectWallet() {
     console.log("connect try", this.network, this.networkInterface);
     if (this.network) await this.networkInterface?.connectWallet();
-  };
+  }
 
-  disconnectWallet = async () => {
+  async disconnectWallet() {
     await this.networkInterface?.disconnectWallet();
-  };
+  }
 
-  onProviderStateChange = (state) => {
+  onProviderStateChange(state) {
     this.emit("providerStateChange", state);
-  };
+  }
 
-  initiateNetworkInteface = (network) => {
+  initiateNetworkInteface(network) {
     const NetworkClass = this.networkClasses[network];
     if (!NetworkClass) throw new Error("Network not found");
     return new NetworkClass({ core: this, signInMessage: this.signInMessage });
-  };
+  }
 
-  stopNetworkInterface = async () => {
+  async stopNetworkInterface() {
     await this.networkInterface?.stop();
-  };
+  }
 
-  getProfile = async (address) => {
-    if (!address) throw new Error("profile request for undefined address");
-    if (!this._profiles[address]) {
-      const profile = (this._profiles[address] = {
-        description: null,
-        website: null,
-        image: null,
-        ENS: null,
-        address,
-      });
-
-      if (!address) {
-        return profile;
-      }
-
-      profile.name = `${address.substr(0, 6)}…${address.substr(-6)}`;
-
-      if (!profile.image) {
-        profile.image = createIcon({ seed: address }).toDataURL();
-        const profileWithIcon = {
-          ...profile,
-          image: createIcon({ seed: address }).toDataURL(),
-        };
-        this._profiles[address] = profileWithIcon;
-      }
-      if (!profile.ENS) {
-        profile.ENS = this._fetchENSName(address);
-      }
-    }
-    return this._profiles[address];
-  };
-
-  _fetchENSName = async (address) => {
-    let ENS = {};
-    try {
-      await getENSName(address).then((res) => (ENS.name = res));
-      if (ENS.name) return ENS;
-      return {};
-    } catch (err) {
-      console.log(`ENS error: ${err}`);
-    }
-  };
-
-  _socketOpen = () => {
-    this.__pingServerTimeout = setInterval(this.ping, 5000);
+  _socketOpen() {
     this.lastSocketOpenState = true;
     this.emit("open");
-  };
+  }
 
-  _socketClose = ({ noRetry }) => {
-    clearInterval(this.__pingServerTimeout);
+  _socketClose({ noRetry }) {
     this.emit("close");
     if (!noRetry) {
       // toast.error("Connection to server closed. Please try again in a minute");
@@ -150,24 +114,24 @@ export default class Core extends Emitter {
         this.lastSocketOpenState ? 0 : 8000
       );
     }
-  };
+  }
 
-  _socketMsg = (e) => {
+  _socketMsg(e) {
     if (!e.data && e.data.length <= 0) return;
     const msg = JSON.parse(e.data);
     this.emit("message", msg.op + "_ws", msg.data);
-  };
+  }
 
-  start = () => {
+  start() {
     if (this.ws) this.stop();
     this.ws = new WebSocket(this.websocketUrl);
     this.ws.addEventListener("open", this._socketOpen);
     this.ws.addEventListener("close", this._socketClose);
     this.ws.addEventListener("message", this._socketMsg);
     this.emit("start");
-  };
+  }
 
-  stop = () => {
+  stop() {
     if (!this.ws) return;
     this.ws.removeEventListener("open", this._socketOpen);
     this.ws.removeEventListener("close", this._socketClose);
@@ -176,19 +140,13 @@ export default class Core extends Emitter {
     this.ws.close();
     this.ws = null;
     this.emit("stop");
-  };
+  }
 
-  getAccountState = async () => {
-    return (await this.networkInterface?.getAccountState()) ?? {};
-  };
-
-  ping = () => this.send("ping");
-
-  send = (op, args) => {
+  send(op, args) {
     return this.ws.send(JSON.stringify({ op, args }));
-  };
+  }
 
-  sendRequest = async (url, method, data, isPrivate = false) => {
+  async sendRequest(url, method, data, isPrivate = false) {
     if (method) method = method.toLowerCase();
     let token;
     if (isPrivate) {
@@ -245,40 +203,40 @@ export default class Core extends Emitter {
         }
         // console.log(error.config);
       });
-  };
+  }
 
-  subscribeToMarket = (market) => {
+  subscribeToMarket(market) {
     this.sendRequest("markets/subscription", "POST", {
       network: this.network,
       market: market,
       uuid: this.ws.uuid,
       clear: true,
     });
-  };
+  }
 
-  unsubscribeToMarket = (market) => {
+  unsubscribeToMarket(market) {
     this.sendRequest("markets/subscription", "DELETE", {
       network: this.network,
       market: market,
       uuid: this.ws.uuid,
     });
-  };
+  }
 
-  getMarketConfig = (market) => {
+  getMarketConfig(market) {
     this.sendRequest("markets/config", "GET", {
       network: this.network,
       markets: market,
     });
-  };
+  }
 
-  getMarketInfo = (market) => {
+  getMarketInfo(market) {
     this.sendRequest("markets/info", "GET", {
       network: this.network,
       markets: market,
     });
-  };
+  }
 
-  cancelOrder = (orderId) => {
+  cancelOrder(orderId) {
     this.sendRequest(
       "user/order",
       "DELETE",
@@ -287,9 +245,9 @@ export default class Core extends Emitter {
       },
       true
     );
-  };
+  }
 
-  cancelAllOrders = async () => {
+  cancelAllOrders() {
     this.sendRequest(
       "user/orders",
       "DELETE",
@@ -300,7 +258,7 @@ export default class Core extends Emitter {
       true
     );
     return true;
-  };
+  }
 
   getOrderBook(market, side) {
     this.sendRequest("orders", "GET", {
@@ -312,15 +270,7 @@ export default class Core extends Emitter {
     });
   }
 
-  submitOrder = async (
-    product,
-    side,
-    price,
-    amount,
-    feeType,
-    fee,
-    orderType
-  ) => {
+  async submitOrder(product, side, price, amount, feeType, fee, orderType) {
     if (!this.isSignedIn()) return;
 
     const data = await this.networkInterface.prepareOrder(
@@ -345,119 +295,15 @@ export default class Core extends Emitter {
       },
       true
     );
-  };
+  }
 
-  getNetworks = () => {
+  getNetworks() {
     this.axiosInstance.get("/networks").then((res) => {
       this.emit("setNetworkList", res.data.networks);
     });
-  };
+  }
 
-  verifiedAccountNonce = async () => {
-    return await this._accountState.verified.nonce;
-  };
-
-  increaseWalletNonce = async () => {
-    let increaseNonceResult = {};
-
-    const increaseNonceRes = await this.networkInterface.increaseWalletNonce();
-    // cancel all orders if wallet nonce is increased
-    this.cancelAllOrders();
-    const verifiedAccountNonce = await this._accountState.verified.nonce;
-    if (increaseNonceRes) {
-      increaseNonceResult.response = increaseNonceRes;
-      increaseNonceResult.verifiedAccountNonce = verifiedAccountNonce;
-    }
-
-    return increaseNonceResult;
-  };
-
-  depositL2 = async (amount, token) => {
-    return this.networkInterface?.depositL2(amount, token);
-  };
-
-  withdrawL2 = async (amount, token) => {
-    return this.networkInterface?.withdrawL2(amount, token);
-  };
-
-  depositL2Fee = async (token) => {
-    return this.networkInterface?.depositL2Fee(token);
-  };
-
-  withdrawL2Fee = async (token) => {
-    return this.networkInterface?.withdrawL2Fee(token);
-  };
-
-  getCommitedBalance = async () => {
-    const commitedBalance = this.networkInterface?.getCommitedBalance();
-    if (commitedBalance) {
-      return commitedBalance;
-    } else {
-      return 0;
-    }
-  };
-
-  getNetworkContract = () => {
-    return this.networkInterface?.BRIDGE_CONTRACT;
-  };
-
-  approveSpendOfCurrency = async (currency, allowance = maxAllowance) => {
-    return await this.networkInterface?.approveSpendOfCurrency(
-      currency,
-      allowance || maxAllowance,
-      erc20ContractABI
-    );
-  };
-
-  getBalanceOfCurrency = async (currency) => {
-    return await this.networkInterface?.getBalanceOfCurrency(
-      currency,
-      erc20ContractABI,
-      maxAllowance
-    );
-  };
-
-  getWalletBalances = async () => {
-    if (!this.networkInterface || !this.networkInterface.apiProvider)
-      return null;
-    const balances = {};
-
-    const getBalance = async (ticker) => {
-      try {
-        const { balance, allowance } = await this.getBalanceOfCurrency(ticker);
-        balances[ticker] = {
-          value: balance,
-          allowance,
-          valueReadable: formatAmount(balance, this.currencies[ticker]),
-        };
-
-        this.emit("balanceUpdate", "wallet", { ...balances });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    let tickers;
-    try {
-      tickers = Object.keys(this.currencies).filter(
-        (ticker) => this.currencies[ticker].chain[this.network]
-      );
-    } catch (err) {
-      return null;
-    }
-
-    await Promise.all(tickers.map((ticker) => getBalance(ticker)));
-
-    return balances;
-  };
-
-  getBalances = async () => {
-    if (!this.networkInterface) return null;
-    const balances = await this.networkInterface.getBalances();
-    this.emit("balanceUpdate", this.network, balances);
-    return balances;
-  };
-
-  getOrderDetailsWithoutFee = (order) => {
+  getOrderDetailsWithoutFee(order) {
     const side = order.side;
     const baseQuantity = new Decimal(order.baseQuantity);
     const price = new Decimal(order.price);
@@ -513,7 +359,7 @@ export default class Core extends Emitter {
       baseQuantity: baseQuantityWithoutFee,
       remaining: remainingWithoutFee,
     };
-  };
+  }
 
   getFillDetailsWithoutFee(fill) {
     const time = fill.insertTimestamp;
@@ -576,7 +422,13 @@ export default class Core extends Emitter {
     }
   }
 
-  isSignedIn = () => {
+  isSignedIn() {
     return sessionStorage.getItem("access_token") !== null;
-  };
+  }
+
+  async run(action, ...args) {
+    if (Core.Actions.includes(action)) return await this[action](...args);
+    if (this.networkInterface?.constructor.Actions?.includes(action))
+      return await this.networkInterface[action](...args);
+  }
 }
