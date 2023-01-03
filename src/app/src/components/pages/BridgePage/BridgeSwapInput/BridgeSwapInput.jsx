c80ce0ab9ce10803acc1e3032272011d385afd82
Butcher <billiyButcher@proton.me>
2023-01-03 17:54:25 +0330

    featur(app):implement new design of bridge desktop size

--- src/app/src/components/pages/BridgePage/BridgeSwapInput/BridgeSwapInput.jsx
+++ src/app/src/components/pages/BridgePage/BridgeSwapInput/BridgeSwapInput.jsx
@@ -1,59 +1,66 @@
 import React, { useCallback } from "react";
-import styled from "@xstyled/styled-components";
 import BridgeCurrencySelector from "../BridgeCurrencySelector/BridgeCurrencySelector";
+import { styled } from "@mui/material";
+import { flexDirection } from "@xstyled/styled-components";
 
-const BridgeInputBox = styled.div`
-  display: flex;
-  border: 2px solid var(--dexpressoPrimery) !important;
-  flex-direction: row;
-  align-items: center;
-  background: #fff;
-  border-radius: 24px;
-  border: none;
-  position: relative;
-  -webkit-appearance: none;
-  appearance: none;
-  width: 100%;
+const BridgeInputForm = styled("div")(() => ({
+  display: "flex",
+  borderBottom: "2px solid var(--dexpressoPrimery) !important",
+  flexDirection: "row",
+  alignItems: "center",
+  border: "none",
+  position: "relative",
+  appearance: "none",
+  width: "100%",
 
-  input,
-  input:focus {
-    // font-family: 'Iceland', sans-serif;
-    width: calc(100% - 96px);
-    background: transparent;
-    padding: 5px 20px;
-    font-size: 28px;
-    border: none;
-    outline: none;
-    text-align: right;
-    -webkit-appearance: none;
-    appearance: none;
+  "& input , input:focus,input::placeholder": {
+    width: "100%",
+    background: "transparent",
+    padding: "5px 20px",
+    fontSize: "28px",
+    border: "none",
+    outline: "none",
+    color: "#fff",
+    textAlign: "left",
+    appearance: "none"
   }
 
-  .maxLink {
-    position: absolute;
-    color: #2f353a;
-    top: -58px;
-    right: 0;
-    padding: 6px 12px;
-    background: rgba(0, 0, 0, 0.3);
-    border-radius: 8px;
-    user-select: none;
+  // .maxLink {
+  //   position: absolute;
+  //   color: #2f353a;
+  //   top: -58px;
+  //   right: 0;
+  //   padding: 6px 12px;
+  //   background: rgba(0, 0, 0, 0.3);
+  //   border-radius: 8px;
+  //   user-select: none;
 
-    &:hover {
-      color: #2f353a;
-    }
-  }
-
-  .currencySelector {
-    width: 37%;
-    height: 52px;
-    display: flex;
-    align-items: center;
-    margin-left: 15px;
-    border-right: 2px solid var(--dexpressoPrimery);
-  }
-`;
-
+  //   &:hover {
+  //     color: #2f353a;
+  //   }
+  // }
+}));
+const CurrencySelector = styled("div")(() => ({
+  width: "180px",
+  height: "55px",
+  display: "flex",
+  alignItems: "center",
+  marginRight: "15px"
+}));
+const BridgeInputBox = styled("div")(() => ({
+  display: "flex",
+  justifyContent:"space-between",
+  flexDirection:"column"
+}));
+const BridgeSwapInputBox = styled("div")(() => ({
+  height: "100%",
+  display: "flex"
+}));
+const CurrencySelectorBox = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between"
+}));
 const BridgeSwapInput = ({
   value = {},
   onChange,
@@ -61,19 +68,16 @@ const BridgeSwapInput = ({
   currencies,
   balances = {},
   className,
+  children,
+  availableBalanceOnSelectedSide
 }) => {
-  const setCurrency = useCallback(
-    (currency) => onChange({ currency, amount: "" }),
-    [onChange]
-  );
+  const setCurrency = useCallback((currency) => onChange({ currency, amount: "" }), [onChange]);
   const setAmount = useCallback(
     (e) => onChange({ amount: e.target.value.replace(/[^0-9.]/g, "") }),
     [onChange]
   );
 
-  let maxBalance = Number(
-    (balances?.[value.currency]?.valueReadable) || 0
-  );
+  let maxBalance = Number(balances?.[value.currency]?.valueReadable || 0);
   maxBalance -= 0.000105;
 
   const setMax = () => {
@@ -83,31 +87,38 @@ const BridgeSwapInput = ({
   };
 
   return (
-    <div className="mt-0 mt-md-5">
-      <h5 className="mb-2">Source coin</h5>
-      <BridgeInputBox>
-        <div className="currencySelector">
-          <BridgeCurrencySelector
-            currencies={currencies}
-            balances={balances}
-            onChange={setCurrency}
-            value={value.currency}
-          />
-        </div>
-        <input
-          onChange={setAmount}
-          value={value.amount}
-          className={className}
-          placeholder="0.00"
-          type="text"
-        />
-        {false && (
-          <a className="maxLink" href="#max" onClick={setMax}>
-            Max
-          </a>
-        )}
-      </BridgeInputBox>
-    </div>
+    <>
+      <BridgeSwapInputBox>
+        <CurrencySelectorBox>
+          {children}
+          <CurrencySelector>
+            <BridgeCurrencySelector
+              currencies={currencies}
+              balances={balances}
+              onChange={setCurrency}
+              value={value.currency}
+            />
+          </CurrencySelector>
+        </CurrencySelectorBox>
+        <BridgeInputBox>
+          {availableBalanceOnSelectedSide}
+          <BridgeInputForm>
+            <input
+              onChange={setAmount}
+              value={value.amount}
+              className={className}
+              placeholder="0.00"
+              type="text"
+            />
+            {false && (
+              <a className="maxLink" href="#max" onClick={setMax}>
+                Max
+              </a>
+            )}
+          </BridgeInputForm>
+        </BridgeInputBox>
+      </BridgeSwapInputBox>
+    </>
   );
 };
 
