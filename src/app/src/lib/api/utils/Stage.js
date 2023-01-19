667d1ae9742c2ba378f785bce74443df64cb5bc6
0xfadecafe <0xfadecafe@proton.me>
2023-01-19 13:28:15 +0330

    Refactor(app): Stage - Added skip functionality on start fixed syntax error added events to reset the stage manager small fix on propagating childs added restart functionality to stage manager

--- src/app/src/lib/api/utils/Stage.js
+++ src/app/src/lib/api/utils/Stage.js
@@ -14,8 +14,8 @@ export class Stage {
   /** @type {[string]} */
   children = [];
 
-  _onStart = () => {};
-  _onFinish = () => {};
+  _onStart;
+  _onFinish;
 
   startPromise;
   finishPromise;
@@ -37,16 +37,24 @@ export class Stage {
    * Single string for triggering finish when the event happenes.
    * Or list of string and a checker function to run the checker whenever the event happenes
    * and trigger finish if the returned value was true.
-   * @param {function} onStart
-   * @param {function} onFinish
+   * @param {function(function)} onStart
+   * Has a function argument that if you want to skip this stage you can call.
+   * @param {function(boolean)} onFinish
+   * Has a boolean argument that shows if the stage was finished by skipping or not.
    * @param {[string]} children
    * @param {string} name
    */
-  constructor(parents = [], eventsToFinish = [], onStart = () => {}, onFinish = () => {}, children = [], name = null) {
+  constructor(
+    parents = [],
+    eventsToFinish = [],
+    onStart = skip => {},
+    onFinish = skipped => {},
+    children = [],
+    name = null,
+  ) {
     this.name = name;
     this.parents = parents || [];
     this.children = children || [];
-    console.log(typeof []);
     this._onStart = onStart || (() => {});
     this._onFinish = onFinish || (() => {});
     this._eventsToFinish = eventsToFinish.map(item => (item instanceof Array ? item : [item, () => true]));
@@ -55,8 +63,8 @@ export class Stage {
 
   reset() {
     this.status = "INITIAL";
-    this._startPromiseReject?.();
-    this._finishPromiseReject?.();
+    // this._startPromiseReject?.();
+    // this._finishPromiseReject?.();
     this.startPromise = new Promise((res, rej) => {
       this._startPromiseResolve = res;
       this._startPromiseReject = rej;
@@ -77,7 +85,7 @@ export class Stage {
    */
   triggerStart(parent = null) {
     if (parent && this.parents.includes(parent)) this.finishedParents.add(parent);
-    if (this.status === "INITIAL" && this.finishedParents.size() === this.parents.length) this._start();
+    if (this.status === "INITIAL" && this.finishedParents.size === this.parents.length) this._start();
   }
 
   // triggerFinish(eventName, data = undefined) {
@@ -87,29 +95,29 @@ export class Stage {
   _addFinishEventHandlers() {
     this._finishEventHandlers = this._eventsToFinish.map(([event, checker]) => [
       event,
-      data => {
-        if (this.status === "RUNNING" && checker(data)) this._finish();
-      },
+      (...data) => this.status === "RUNNING" && checker(...data) && this._finish(),
     ]);
     this._finishEventHandlers.forEach(([event, handler]) => this.eventSource?.on(event, handler));
   }
 
   _removeFinishEventHandlers() {
-    this._finishEventHandlers.forEach(([event, handler]) => this.eventSource?.off(event, handler));
+    this._finishEventHandlers?.forEach(([event, handler]) => this.eventSource?.off(event, handler));
   }
 
   _start() {
     this._addFinishEventHandlers();
     this.status = "RUNNING";
     this._startPromiseResolve();
-    this._onStart?.();
+    this._onStart?.(() => {
+      this._finish(true);
+    });
   }
 
-  _finish() {
+  _finish(skipped = false) {
     this._removeFinishEventHandlers();
     this.status = "FINISHED";
     this._finishPromiseResolve();
-    this._onFinish?.();
+    this._onFinish?.(skipped);
     this.eventSource?.emit("STAGE_FINISHED", this.name);
   }
 
@@ -162,8 +170,9 @@ export class StageManager {
   /**
    * @param {{name:Stage}} stages
    * @param {string} entry Starting stage node
+   * @param {[string | [event: string, checker: function(object):boolean]]} eventsToReset
    */
-  constructor(stages, entry = null) {
+  constructor(stages, entry = null, eventsToReset = []) {
     this.stages = Object.fromEntries(Object.entries(stages).map(([name, stage]) => [name, stage.copy(name)]));
     this.entry = this.stages[entry] && entry;
     Object.values(this.stages).forEach(stage => {
@@ -173,27 +182,39 @@ export class StageManager {
     });
     Object.values(this.stages).forEach(stage => {
       stage.parents.forEach(parent => {
-        this.stages[parent].addChild(parent);
+        this.stages[parent].addChild(stage);
       });
     });
+    this._eventsToRestart = eventsToReset.map(item => (item instanceof Array ? item : [item, () => true]));
     this.reset();
   }
 
   reset() {
-    Object.values(this.stages).forEach(stage => stage._reset());
+    Object.values(this.stages).forEach(stage => stage.reset());
     this.emitter = new EventEmitter();
     Object.values(this.stages).forEach(stage => stage.setEventSource(this.emitter));
-    this.emitter.on("STAGE_FINISHED", stage => this.stages[stage].triggerStart(stage));
+    this.emitter.on("STAGE_FINISHED", stage =>
+      this.stages[stage].children.forEach(child => this.stages[child]?.triggerStart(stage)),
+    );
+    this._eventsToRestart.forEach(([event, checker]) =>
+      this.emitter.on(event, (...data) => checker(...data) && this.restart()),
+    );
   }
 
   /**
    * @param {string?} entry Starting stage node
    * @throws Will throw an error when no source of an entry point was found.
-  */
+   */
   start(entry = null) {
     entry = (this.stages[entry] && entry) || this.entry;
     if (!entry) throw new Error("No entry point found to start the staging process");
+    this.entry = entry;
     this.stages[entry].triggerStart();
+  }
+
+  restart() {
+    this.reset();
+    this.start();
   }
 
   emit(event, ...args) {
