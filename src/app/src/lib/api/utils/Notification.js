261a5e8ad0db56f49ebde9b76291e1ae49c61a71
0xfadecafe <0xfadecafe@proton.me>
2023-02-07 20:28:07 +0330

    Feature(app): Saving toast ids to store Clearing all notifs method Getting all stored notifs method

--- src/app/src/lib/api/utils/Notification.js
+++ src/app/src/lib/api/utils/Notification.js
@@ -1,3 +1,4 @@
+import { notificationsSelector } from "lib/store/features/api/apiSlice";
 import Core from "../Core";
 
 const { toast } = require("react-toastify");
@@ -26,16 +27,34 @@ class NotificationSystem {
 
   static add(method, msg) {
     const id = this._random();
-    Core.run("emit", "notifications", "add", id, method, msg);
-    toast[method](msg);
+    const notifToast = toast[method](msg);
+    console.log("notifToast", notifToast);
+    Core.run("emit", "notifications", "add", id, method, msg, notifToast);
+    return id;
   }
 
   static remove(id) {
+    const notif = this._getNotifications().find(item => item.id === id);
+    console.log("notif", id, notif);
+    if (notif) {
+      if (notif.toast)
+        toast.dismiss(notif.toast);
+    }
     Core.run("emit", "notifications", "remove", id);
+  }
+
+  static clear() {
+    Core.run("emit", "notifications", "clear");
   }
 
   static _random() {
     return new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
+  }
+
+  static _getNotifications() {
+    const state = Core.getInstance().store.getState();
+    console.log("state", state);
+    return notificationsSelector(state);
   }
 
 }
